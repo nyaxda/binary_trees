dc86c6f18e0c3fed3923873ea9cf7d85858469d8
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-26 11:24:07 -0700

    Betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_height_helper - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: height of the tree or 0 if tree is NULL
  */
@@ -26,18 +26,20 @@ size_t binary_tree_height_helper(const binary_tree_t *tree)
 		else
 			return (right_height);
 	}
-	return 0;
+	return (0);
 }
 size_t binary_tree_height_helper(const binary_tree_t *tree);
 
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
+ * binary_tree_balance - function that measures the balance
+ * factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  * Return: balance factor of the tree
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_height_helper(tree->left) - binary_tree_height_helper(tree->right));
+		return (binary_tree_height_helper(tree->left) -
+		binary_tree_height_helper(tree->right));
 	return (0);
 }
