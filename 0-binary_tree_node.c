7c81c31e62f4adeafb58aa343bcf4b941a42cd68
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-26 04:50:37 -0700

    Error fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,13 +6,15 @@
  * @value: value to put in the new node
  * Return: pointer to the new node, or NULL on failure
 */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    parent = malloc(sizeof(struct binary_tree_s));
-    if (parent == NULL)
-        return (NULL);
-    parent->n = value;
-    parent->left = NULL;
-    parent->right = NULL;
-    return (parent);
+	binary_tree_t *new_node = malloc(sizeof(struct binary_tree_s));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+	return (new_node);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
